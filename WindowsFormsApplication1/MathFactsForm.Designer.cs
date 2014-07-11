7e749aac13fddd92b3d38786231b124980e0e327
creativebluebutterfly <kbroskie@gmail.com>
2014-07-11 13:43:06 -0400

    Fixed form size

--- WindowsFormsApplication1/MathFactsForm.Designer.cs
+++ WindowsFormsApplication1/MathFactsForm.Designer.cs
@@ -28,25 +28,25 @@
 		/// </summary>
 		private void InitializeComponent ()
 		{
-			this.components = new System.ComponentModel.Container ();
-			this.mainMenu = new System.Windows.Forms.MainMenu (this.components);
-			this.dailyFactsMenuItem = new System.Windows.Forms.MenuItem ();
-			this.additionFactsMenuItem = new System.Windows.Forms.MenuItem ();
-			this.subtractionMenuItem = new System.Windows.Forms.MenuItem ();
-			this.multiplicationMenuItem = new System.Windows.Forms.MenuItem ();
-			this.divisionFactsMenuItem = new System.Windows.Forms.MenuItem ();
-			this.speedTestMenuItem = new System.Windows.Forms.MenuItem ();
-			this.additionTestMenuItem = new System.Windows.Forms.MenuItem ();
-			this.subtractionTestMenuItem = new System.Windows.Forms.MenuItem ();
-			this.multiplicationTestMenuItem = new System.Windows.Forms.MenuItem ();
-			this.divisionTestMenuItem = new System.Windows.Forms.MenuItem ();
-			this.optionsMenuItem = new System.Windows.Forms.MenuItem ();
-			this.helpMenuItem = new System.Windows.Forms.MenuItem ();
-			this.SuspendLayout ();
+			this.components = new System.ComponentModel.Container();
+			this.mainMenu = new System.Windows.Forms.MainMenu(this.components);
+			this.dailyFactsMenuItem = new System.Windows.Forms.MenuItem();
+			this.additionFactsMenuItem = new System.Windows.Forms.MenuItem();
+			this.subtractionMenuItem = new System.Windows.Forms.MenuItem();
+			this.multiplicationMenuItem = new System.Windows.Forms.MenuItem();
+			this.divisionFactsMenuItem = new System.Windows.Forms.MenuItem();
+			this.speedTestMenuItem = new System.Windows.Forms.MenuItem();
+			this.additionTestMenuItem = new System.Windows.Forms.MenuItem();
+			this.subtractionTestMenuItem = new System.Windows.Forms.MenuItem();
+			this.multiplicationTestMenuItem = new System.Windows.Forms.MenuItem();
+			this.divisionTestMenuItem = new System.Windows.Forms.MenuItem();
+			this.optionsMenuItem = new System.Windows.Forms.MenuItem();
+			this.helpMenuItem = new System.Windows.Forms.MenuItem();
+			this.SuspendLayout();
 			// 
 			// mainMenu
 			// 
-			this.mainMenu.MenuItems.AddRange (new System.Windows.Forms.MenuItem[] {
+			this.mainMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
             this.dailyFactsMenuItem,
             this.speedTestMenuItem,
             this.optionsMenuItem,
@@ -55,7 +55,7 @@
 			// dailyFactsMenuItem
 			// 
 			this.dailyFactsMenuItem.Index = 0;
-			this.dailyFactsMenuItem.MenuItems.AddRange (new System.Windows.Forms.MenuItem[] {
+			this.dailyFactsMenuItem.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
             this.additionFactsMenuItem,
             this.subtractionMenuItem,
             this.multiplicationMenuItem,
@@ -67,33 +67,33 @@
 			this.additionFactsMenuItem.Index = 0;
 			this.additionFactsMenuItem.Shortcut = System.Windows.Forms.Shortcut.CtrlA;
 			this.additionFactsMenuItem.Text = "Start Addition Facts";
-			this.additionFactsMenuItem.Click += new System.EventHandler (this.dailyFactsClick);
+			this.additionFactsMenuItem.Click += new System.EventHandler(this.dailyFactsClick);
 			// 
 			// subtractionMenuItem
 			// 
 			this.subtractionMenuItem.Index = 1;
 			this.subtractionMenuItem.Shortcut = System.Windows.Forms.Shortcut.CtrlS;
 			this.subtractionMenuItem.Text = "Start Subtraction Facts";
-			this.subtractionMenuItem.Click += new System.EventHandler (this.dailyFactsClick);
+			this.subtractionMenuItem.Click += new System.EventHandler(this.dailyFactsClick);
 			// 
 			// multiplicationMenuItem
 			// 
 			this.multiplicationMenuItem.Index = 2;
 			this.multiplicationMenuItem.Shortcut = System.Windows.Forms.Shortcut.CtrlM;
 			this.multiplicationMenuItem.Text = "Start Multiplication Facts";
-			this.multiplicationMenuItem.Click += new System.EventHandler (this.dailyFactsClick);
+			this.multiplicationMenuItem.Click += new System.EventHandler(this.dailyFactsClick);
 			// 
 			// divisionFactsMenuItem
 			// 
 			this.divisionFactsMenuItem.Index = 3;
 			this.divisionFactsMenuItem.Shortcut = System.Windows.Forms.Shortcut.CtrlD;
 			this.divisionFactsMenuItem.Text = "Start Division Facts";
-			this.divisionFactsMenuItem.Click += new System.EventHandler (this.dailyFactsClick);
+			this.divisionFactsMenuItem.Click += new System.EventHandler(this.dailyFactsClick);
 			// 
 			// speedTestMenuItem
 			// 
 			this.speedTestMenuItem.Index = 1;
-			this.speedTestMenuItem.MenuItems.AddRange (new System.Windows.Forms.MenuItem[] {
+			this.speedTestMenuItem.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
             this.additionTestMenuItem,
             this.subtractionTestMenuItem,
             this.multiplicationTestMenuItem,
@@ -104,25 +104,25 @@
 			// 
 			this.additionTestMenuItem.Index = 0;
 			this.additionTestMenuItem.Text = "Addition Speed Test";
-			this.additionTestMenuItem.Click += new System.EventHandler (this.speedTestClick);
+			this.additionTestMenuItem.Click += new System.EventHandler(this.speedTestClick);
 			// 
 			// subtractionTestMenuItem
 			// 
 			this.subtractionTestMenuItem.Index = 1;
 			this.subtractionTestMenuItem.Text = "Subtraction Speed Test";
-			this.subtractionTestMenuItem.Click += new System.EventHandler (this.speedTestClick);
+			this.subtractionTestMenuItem.Click += new System.EventHandler(this.speedTestClick);
 			// 
 			// multiplicationTestMenuItem
 			// 
 			this.multiplicationTestMenuItem.Index = 2;
 			this.multiplicationTestMenuItem.Text = "Multiplication Speed Test";
-			this.multiplicationTestMenuItem.Click += new System.EventHandler (this.speedTestClick);
+			this.multiplicationTestMenuItem.Click += new System.EventHandler(this.speedTestClick);
 			// 
 			// divisionTestMenuItem
 			// 
 			this.divisionTestMenuItem.Index = 3;
 			this.divisionTestMenuItem.Text = "Division Speed Test";
-			this.divisionTestMenuItem.Click += new System.EventHandler (this.speedTestClick);
+			this.divisionTestMenuItem.Click += new System.EventHandler(this.speedTestClick);
 			// 
 			// optionsMenuItem
 			// 
@@ -137,14 +137,14 @@
 			// 
 			// MathFactsForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF (6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.FromArgb (((int) (((byte) (255)))), ((int) (((byte) (192)))), ((int) (((byte) (255)))));
-			this.ClientSize = new System.Drawing.Size (290, 215);
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+			this.ClientSize = new System.Drawing.Size(290, 257);
 			this.Menu = this.mainMenu;
 			this.Name = "MathFactsForm";
 			this.Text = "Know Your Facts";
-			this.ResumeLayout (false);
+			this.ResumeLayout(false);
 
 		}
 
